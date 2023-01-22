616614b1eeec308f955c4c6e5cc4e60f08739c11
Fokusmod <Info@mail.ru>
2023-01-22 14:38:21 +0300

    Added a few events Listeners -MessageListener -MessageChangeListener -MessageDeleteListener
    Create MessageService for processing requests:
    -getMessageMethod
    -postMessageInChannel
    -changeMessageMethod
    -deleteMessageMethod (in work)

--- SlackIntegrationService/src/main/java/ru/geekbrains/slackintegrationservice/config/SlackConfig.java
+++ SlackIntegrationService/src/main/java/ru/geekbrains/slackintegrationservice/config/SlackConfig.java
@@ -1,68 +1,57 @@
 package ru.geekbrains.slackintegrationservice.config;
 
 
+
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.AppConfig;
-import com.slack.api.methods.request.chat.ChatPostMessageRequest;
-import com.slack.api.model.event.MessageBotEvent;
-import com.slack.api.model.event.MessageEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
+
+
 
 
 @Configuration
+@RequiredArgsConstructor
 public class SlackConfig {
 
-//    private final String CLIENT_ID = "1128391916193.4643061959205";
-//    private final String CLIENT_SECRET = "cf6f33912333ee5d367ed6a6b25da0a1";
-//    private final String SIGNING_SECRET = "27256d9f2860422ef096a3cf89daef6c";
 
-    // If you would like to run this app for a single workspace,
-    // enabling this Bean factory should work for you.
-//    @Bean
-//    public AppConfig loadSingleWorkspaceAppConfig() {
-//        return AppConfig.builder()
-//                .singleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"))
-//                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
-//                .build();
-//    }
+    //TODO Поменять на переменные окружения.
+    @Bean
+    public AppConfig loadSingleWorkspaceAppConfig() {
+        return AppConfig.builder()
+                .singleTeamBotToken("xoxb-4639953086342-4664911587190-QKHC84uqViOeIwpv4Ry2K8It")
+                .signingSecret("fb185ea73d08a1d32bf7ebeafaecffb6")
+                .build();
+    }
 
     // If you would like to run this app for multiple workspaces,
     // enabling this Bean factory should work for you.
-    @Bean
-    public AppConfig loadOAuthConfig() {
-        return AppConfig.builder()
+//    @Bean
+//    public AppConfig loadOAuthConfig() {
+//        return AppConfig.builder()
 //                .singleTeamBotToken(null)
-                .singleTeamBotToken(System.getenv("SLACK_BOT_TOKEN"))
-                .clientId(System.getenv("SLACK_CLIENT_ID"))
-                .clientSecret(System.getenv("SLACK_CLIENT_SECRET"))
-                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
-                .scope("app_mentions:read,channels:history,channels:read,groups:history,groups:read,incoming-webhook")
-                .oauthInstallPath("/slack/install")
-                .oauthRedirectUriPath("/slack/oauth_redirect")
-                .build();
-    }
+//                .clientId(System.getenv("SLACK_CLIENT_ID"))
+//                .clientSecret(System.getenv("SLACK_CLIENT_SECRET"))
+//                .signingSecret(System.getenv("SLACK_SIGNING_SECRET"))
+//                .scope("app_mentions:read,channels:history,chat:write,groups:history,commands,im:history")
+//                .oauthInstallPath("/slack/install")
+//                .oauthRedirectUriPath("/slack/oauth_redirect")
+//                .build();
+//    }
 
     @Bean
     public App initSlackApp(AppConfig config) {
         App app = new App(config);
-        if (config.getClientId() != null) {
-            app.asOAuthApp(true);
-        }
 
-        app.command("/hello", (req, ctx) -> {
-            return ctx.ack(r -> r.text("Thanks!"));
-        });
+        //На всякий случай чтобы не запустились настройки для нескольких рабочих пространств.
+        app.config().setClientId(null);
+        app.config().setClientSecret(null);
 
+//        if (config.getClientId() != null) {
+//            app.asOAuthApp(true);
+//        }
         return app;
     }
 }
-
-
-//"id": "C04JL0F56G7",
-//"name": "тестовое-приложение",
-
-
-
